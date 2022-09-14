d9c7d33f8a80c8fde263332edb214d128e233b11
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-09-14 19:09:32 +0300

    use hooks like useeffect to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router';
 import Card from './component/Card/Card';
+import ViewQuotes from './component/ViewQuotes/ViewQuotes';
 import NavBar from './NavBar/NavBar';
-// import
+import Home from './Home/Home';
 
-function App(){
+function App() {
+  const[quotes, setQuotes] = useState([])
+
+  useEffect(() => {
+    fetch('https://iquotes-app.herokuapp.com/quotes')
+      .then((r) => r.json())
+      .then(data => setQuotes(data))
+  }, [])
+  console.log(quotes);
+
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/navbar" element={<NavBar/>}/>
-        <Route path="/viewquote" element={<ViewQuote/>}/>
+        <Route path="/viewquote" element={<ViewQuotes/>}/>
         <Route path="/card" element={<Card/>}/>
       </Routes>
     </div>
