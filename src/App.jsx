05de78e77bf5d435eadf8e87cde8bdd937126a23
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-09-15 11:32:55 +0300

    import css file to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Card from './components/Card/Card';
