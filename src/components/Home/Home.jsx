dcd3de71bfe1a0d9ed70499b6757a43d612392d1
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-09-15 13:56:07 +0300

    give the div a classname

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,10 +3,8 @@ import './Home.css'
 
 function Home() {
     return (
-        <div>
-            <h1>The Quote Hub</h1>
-            <p>The one-stop place for all wise quotes. | &#129299</p>
-
+        <div className="home">
+            <p>The one-stop place for all wise quotes. | You quote, we post. </p>
             <img src="https://t3.ftcdn.net/jpg/04/44/47/56/360_F_444475661_R1esASlQGVu8IbInW0dkhg7l3LpNofLr.jpg"/>         
         </div>
     )
