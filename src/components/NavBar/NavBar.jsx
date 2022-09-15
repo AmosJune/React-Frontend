7a41b5b999bd8c5253ff670a5ae79a8c30bf4521
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-09-15 13:59:54 +0300

    edit the nav file

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,8 +8,8 @@ function NavBar() {
             <h1>IQuote Hub</h1>
             <li> <img src='https://media.istockphoto.com/vectors/vector-flat-cute-brown-owl-sitting-on-branch-isolated-on-white-vector-id907874662?k=20&m=907874662&s=612x612&w=0&h=xMiE6FnAGChcsj7DPLFwnjv44qFrW-7ESU7e8ts74J0='/></li>
             <li> <Link to="/" >Home</Link> </li>
-            <li> <Link to="/login" >Login</Link> </li>
             <li> <Link to="/authors" >Authors</Link> </li>
+            <li> <Link to="/login" >Login</Link> </li>
         </ul>
     )
 }
